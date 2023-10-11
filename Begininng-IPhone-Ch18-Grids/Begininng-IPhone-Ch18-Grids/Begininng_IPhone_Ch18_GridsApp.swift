0527fd4e07ed7a06ac335934b9d15163884ef063
Tori Lew <55900873+Tori-Lew@users.noreply.github.com>
2023-10-11 15:52:44 -0700

    Add Project Files

---
+++ Begininng-IPhone-Ch18-Grids/Begininng-IPhone-Ch18-Grids/Begininng_IPhone_Ch18_GridsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Begininng_IPhone_Ch18_GridsApp.swift
+//  Begininng-IPhone-Ch18-Grids
+//
+//  Created by Student Account on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Begininng_IPhone_Ch18_GridsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
